b862c527ca3530f1771fb5cbea40fcdcd4b3b5d2
thisisismail <simail.official@gmail.com>
2022-11-14 05:20:27 +0700

    environment variable fixing

--- next.config.js
+++ next.config.js
@@ -12,15 +12,14 @@ const nextConfig = {
   //   ];
   // },
   env: {
-    apiKey: 'AIzaSyAXM39cqFGaeTtAMiePxRw9L8RPZTqVf2Y',
-    authDomain: 'smart-cabinet-747fd.firebaseapp.com',
-    projectId: 'smart-cabinet-747fd',
-    storageBucket: 'smart-cabinet-747fd.appspot.com',
-    messagingSenderId: '82524609873',
-    appId: '1:82524609873:web:2b44aae71c7ee402d2c770',
-    measurementId: 'G-6LKKN60WTL',
-    databaseURL:
-      'https://smart-cabinet-747fd-default-rtdb.asia-southeast1.firebasedatabase.app'
+    apiKey: process.env.APIKEY,
+    authDomain: process.env.AUTHDOMAIN,
+    projectId: process.env.PROJECTID,
+    storageBucket: process.env.STORAGEBUCKET,
+    messagingSenderId: process.env.MESSAGINGSENDERID,
+    appId: process.env.APPID,
+    measurementId: process.env.MEASUREMENTID,
+    databaseURL: process.env.DATABASEURL
   }
 };
 
