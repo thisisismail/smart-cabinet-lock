95cc031550dd6dc29096da650b8c8c7e66bcaf7f
thisisismail <simail.official@gmail.com>
2022-11-24 23:38:47 +0700

    error unused value fixed 2

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import { Typography } from '@material-tailwind/react';
-import LoginCard from '../components/LoginCard';
+// import LoginCard from '../components/LoginCard';
 import Center from '../components/layouts/Centering';
-import { useUser } from '../context/user';
+// import { useUser } from '../context/user';
 
 export default function Home() {
-  const user = useUser();
-  const { uid } = user;
+  // const user = useUser();
+  // const { uid } = user;
 
   return (
     <>
